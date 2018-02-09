cc8879125957b6f35831f3ee70bc0c3793a4629a
Xinyao Wang <wxy26wxy@gmail.com>
2018-02-09 15:44:14 -0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  consumer_key:         'YCLbFqi3oPMh5tTItI6JkNcGZ',
-  consumer_secret:      'n9XvRDk8gtVPq2SOLb9oIiJIOAb3pOM9FTBvU2f3NNlrqYwjVU',
-  access_token:         '789240640177668096-7gcv92ptu7o6yWYvdQZPCAHzeKEtni4',
-  access_token_secret:  'ya4NS3mnyNAGrFoOrfGsQjC3dEv5UCj4cHtJ9D4VpN7IN'
-}
